c1cd285e5ab3bed331529d953dc1732836995c1a
matata samuel <mattasamuel3@gmail.com>
2020-07-25 16:54:04 +0300

    inserted click method on first image

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 //jquery functions for what we do
 $(document).ready(function(){
-    
+    $("#design-img").click(function(){
+        $("#design-p").hide(1300).toggle();
+        $("#design-img").slideup();
+    });
 
 });
