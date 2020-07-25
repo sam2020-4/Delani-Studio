f2a5f4d216abc70089f21c2d3cedc939a278867d
matata samuel <mattasamuel3@gmail.com>
2020-07-25 17:41:56 +0300

    added jquery function for the column 2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
 //jquery functions for what we do
+//first column
 $(document).ready(function(){
     $("#design-img").click(function(){
-        $("#design-p").hide(1300).toggle();
-        $("#design-img").slideup();
+        $(".design-p").hide(500).toggle();
+        $("#design-img").slideUp();    
+            });
+    $(".design-p").click(function(){
+        $("#design-img").slideDown();
+        $(".design-p").hide();
     });
+    
+    //second column
+    $("#dev-img").click(function(){
+        $(".dev-p").hide(500).toggle();
+        $("#dev-img").slideUp();    
+            });
+        $(".dev-p").click(function(){
+            $("#dev-img").slideDown();
+            $(".dev-p").hide();
+        });
+    
+    //third column
+    $("#product-img").click(function(){
+        $(".product-p").hide(500).toggle();
+        $("#product-img").slideUp();    
+            });
+        $(".product-p").click(function(){
+            $("#product-img").slideDown();
+            $(".product-p").hide();
+        });
+    
+    
+
+
 
 });
